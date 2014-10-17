65318f63b0c3f277a8a5d6e5d6d34f6764174a5a
Thomas Omans <thomas.omans@gmail.com>
2014-10-17 02:05:54 -0700

    initial port of python project :cyclone:

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [me.raynes/fs "1.4.4"]
+                 [me.raynes/fs "1.4.6"]
                  [circleci/clj-yaml "0.5.3"]
                  [stencil "0.3.5"]
-                 [matross/mapstache "0.3.0"]
+                 [matross/mapstache "0.3.1-SNAPSHOT"]
                  [compojure "1.1.9"]]
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler oroboros.handler/app}
--- src/oroboros/core.clj
+++ src/oroboros/core.clj
@@ -1,12 +1,67 @@
 (ns oroboros.core
   (:require [matross.mapstache :refer [string-renderer mapstache]]
+            [me.raynes.fs :refer [file iterate-dir normalized parent expand-home split-ext]]
+            [clj-yaml.core :refer [parse-string]]
             [stencil.core :refer [render-string]]))
 
-(defn- mustache
+(defn mustache
   "Use stencil for our mustache impl"
   [str vars]
-  (render-string str vars :replace-missing-vars false))
+  (render-string str vars :missing-var-fn :ignore))
 
 (def template-map
-  "Special map that uses itself for templating context of its string values"
+  "Special map that uses itself for the templating context of its string values"
   (partial mapstache (string-renderer mustache)))
+
+(def default-name "config")
+
+(defn default? [f]
+  (re-matches (re-pattern (str default-name ".*")) f))
+
+(defn find-config-files
+  "return the paths to configs named by the directory"
+  [dir & confs]
+  (let [exts ["yml" "yaml" "json"]
+        confs (cons default-name confs)
+        names (into #{} (for [conf confs ext exts] (str conf "." ext)))
+        sortfn (fn [f]
+                 (let [name (first (split-ext f))]
+                   (if (default? f) -1 (.indexOf confs name))))]
+    (for [[root _ files] (-> dir expand-home iterate-dir)
+          config (sort-by sortfn (clojure.set/intersection files names))] 
+      (file root config))))
+
+(defn config-to-cursor
+  "~/dir, ~/dir/foo/bar/baz.txt => ['foo' 'bar']"
+  [dir conf]
+  (let [dir (-> dir expand-home normalized .getAbsolutePath)
+        conf (-> conf expand-home normalized parent .getAbsolutePath)
+        file-part (subs conf (count dir))
+        splits (clojure.string/split
+                (apply str (rest file-part))
+                (re-pattern java.io.File/separator))]
+    (if (= file-part "") []
+      (map keyword splits))))
+
+(defn load-config
+  "Load a template-map config file from disk"
+  [dir conf]
+  (let [config* (-> conf slurp parse-string template-map)
+        config (into {} config*)
+        cursor (config-to-cursor dir conf)]
+    (if (empty? cursor) config
+        (assoc-in nil cursor config))))
+
+(defn deep-merge
+  "Recursively merges maps. If keys are not maps, the last value wins."
+  [& vals]
+  (if (every? map? vals)
+    (apply merge-with deep-merge vals)
+    (last vals)))
+
+(defn circle-config
+  "Load self referential configs from a directory, named by confs..."
+  [dir & confs]
+  (let [files (apply find-config-files dir confs)
+        configs (map (partial load-config dir) files)]
+    (into {} (template-map (apply deep-merge configs)))))

7affad4fff830aa85b1c89b3519d10f1ff4b29cd
Thomas Omans <thomas.omans@gmail.com>
2015-02-28 11:04:07 -0800

    cleaning up java example :coffee:

--- examples/Example.java
+++ examples/Example.java
@@ -2,10 +2,10 @@ import oroboros.Config;
 
 public class Example {
     public static void main(String[] args) {
-        Config config = new Config();
-        config = config.set("name", "{{cat}} & {{mouse}}");
-        config = config.set("cat", "tom");
-        config = config.set("mouse", "jerry");
+        Config config = new Config()
+            .set("name", "{{cat}} & {{mouse}}")
+            .set("cat", "tom")
+            .set("mouse", "jerry");
         assert config.get("name").equals("tom & jerry");
     }
 }
